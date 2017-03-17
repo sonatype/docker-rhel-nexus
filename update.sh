02e1d7e0586ae6d2c4df67625269c4d8630ec86b
Justin Young <jyoung@sonatype.com>
2017-03-16 17:18:34 -0700

    Folder Restructure with Update Script  (#26)
    * Folders restructured to support various base images. Currently supports CentOS and RHEL
    * Update script to ease incremental version updates

---
+++ update.sh
@@ -0,0 +1,48 @@
+while getopts ":n:s:" opt; do
+  case $opt in
+    n) nexusVersion="$OPTARG";;
+    s) nexusVersionShort="$OPTARG";;
+  esac
+done
+
+if [[ -z "${nexusVersion+set}" ]]; then
+  echo 'Nexus Version must be provided with -n'
+  exit 1
+fi
+
+if [[ -z "${nexusVersionShort+set}" ]]; then
+  echo 'Nexus Version Short (Release Name) must be provided with -s'
+  exit 1
+fi
+
+if ! [ -x "$(command -v md2man-roff)" ]; then
+  echo 'md2man-roff must be installed to use this update script'
+  exit 1
+fi
+
+sedStr="
+  s!%%NEXUS_VERSION%%!$nexusVersion!g;
+  s!%%NEXUS_VERSION_SHORT%%!$nexusVersionShort!g;
+"
+
+# Variants that are not compatible with TravisCI
+for variant in rhel; do
+  if [ ! -d "$nexusVersionShort/$variant" ]; then
+    mkdir -p $nexusVersionShort/$variant
+  fi
+  sed -e "$sedStr" "Dockerfile-$variant.template" > $nexusVersionShort/$variant/Dockerfile
+  md2man-roff help.md > $nexusVersionShort/$variant/help1
+done
+
+travisEnv=
+for variant in centos; do
+  if [ ! -d "$nexusVersionShort/$variant" ]; then
+    mkdir -p $nexusVersionShort/$variant
+  fi
+  sed -e "$sedStr" "Dockerfile-$variant.template" > $nexusVersionShort/$variant/Dockerfile
+  md2man-roff help.md > $nexusVersionShort/$variant/help1
+  travisEnv='\n  - VERSION='"$nexusVersionShort VARIANT=$variant$travisEnv"
+done
+
+travis="$(awk -v 'RS=\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
